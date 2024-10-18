de9a35368024c985aa923ea712c552503e0a8820
dhkim0821 <alex.d.kim0821@gmail.com>
2024-10-18 09:17:02 -0400

    run staccatoe

--- DARoS-Ctrl/main.cpp
+++ DARoS-Ctrl/main.cpp
@@ -4,7 +4,8 @@
 // #include <stdexcept>
 
 int main(int argc, char** argv) {
-    MujocoSimulationBridge* sim_bridge = new PrestoeMJSimulationBridge(THIS_COM"/Prestoe/prestoe.xml");
+    // MujocoSimulationBridge* sim_bridge = new PrestoeMJSimulationBridge();
+    MujocoSimulationBridge* sim_bridge = new StaccatoeMJSimulationBridge();
     sim_bridge->setCtrl();
     sim_bridge->run();
     
